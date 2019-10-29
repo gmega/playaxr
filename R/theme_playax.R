55ebf4059a6c71119f503f4bda65b7ee4c9fcc22
gmega <giuliano.mega@gmail.com>
2019-10-28 21:27:53 -0300

    add font size option

--- R/theme_playax.R
+++ R/theme_playax.R
@@ -12,9 +12,12 @@
 #'   theme_playax()
 #'
 #' @export
-theme_playax <- function() {
+theme_playax <- function(font_size = 12) {
   theme_gray() + theme(
-    text = element_text(family = 'Open Sans'),
+    text = element_text(family = 'Open Sans', size = font_size),
+    axis.text = element_text(size = font_size),
+    axis.title = element_text(size = font_size),
+    legend.text = element_text(size = font_size),
     panel.background = element_rect(fill = NA, colour = NA),
     panel.grid.major.y = element_line(size = rel(1), colour = '#e6e6e6'),
     panel.grid.minor.y = element_blank()
