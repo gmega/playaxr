e69033825b5013b221167fa57a48bbcbcfcf2a5e
gmega <giuliano.mega@gmail.com>
2020-04-28 10:52:21 -0300

    add diff-based rounding to abbrv_large

--- R/abbrv_large.R
+++ R/abbrv_large.R
@@ -11,7 +11,8 @@
 #'  abbrv_large(c(123456, 28475777, 125, 1.3, 0.1, 1))
 #'
 #' @export
-abbrv_large <- function(x, decimals = list()) {
+abbrv_large <- function(x, decimals = NULL) {
+  decimals <- if (is.null(decimals)) compute_decimals(x) else decimals
   x_str <- rep(NA_character_, length(x))
   for (i in 1:nrow(RANGES)) {
     unit <- RANGES[i,]$unit
@@ -26,14 +27,35 @@ abbrv_large <- function(x, decimals = list()) {
 }
 
 abbrv_range <- function(x, magnitude, unit, decimals) {
-  if_else(abs(x) >= magnitude & abs(x) < magnitude * 1000,
+  abs_x <- abs(x)
+  if_else(magnitude <= abs_x & abs_x < magnitude * 1000,
           sprintf('%s%s', as.character(
             truncate(x / magnitude, decimals = decimals)), unit),
           NA)
 }
 
+compute_decimals <- function(x) {
+  decimals <- list()
+  # TODO This makes things easier, but we should actually apply separate
+  # truncations for positive and negative numbers.
+  abs_x <- abs(x[!is.na(x)])
+  mags <- c(RANGES$magnitude, +Inf)
+  units <- RANGES$unit
+
+  for (i in 1:nrow(RANGES)) {
+    x_range <- abs_x[(mags[i] <= abs_x & abs_x < mags[i + 1])]
+    if (length(x_range) <= 1) {
+      next
+    }
+    diffs <- round(diff(sort(unique(x_range))))
+    decimals[units[i]] <- max(0, log10(mags[i]) - min(floor(log10(diffs))))
+  }
+
+  decimals
+}
+
 RANGES <- data.frame(
-  magnitude = c(1e9, 1e6, 1e3),
-  unit = c('B', 'M', 'k'),
+  magnitude = c(1e3, 1e6, 1e9),
+  unit = c('k', 'M', 'B'),
   stringsAsFactors = FALSE
 )
--- tests/testthat/test-abbrv_large.R
+++ tests/testthat/test-abbrv_large.R
@@ -1,3 +1,11 @@
+
+run_comparison <- function(x) {
+  x_num <- unlist(x[c(TRUE, FALSE)])
+  x_str <- unlist(x[c(FALSE, TRUE)])
+
+  expect_equal(abbrv_large(x_num), x_str)
+}
+
 test_that('abbreviating formatter works', {
   x <- list(
     3e9, '3B',
@@ -5,9 +13,9 @@ test_that('abbreviating formatter works', {
     1e9, '1B',
     9e8, '900M',
     13234956, '13M',
-    248576, '248k',
-    -248577, '-248k',
-    1234, '1k',
+    248576, '248.576k',
+    -248577, '-248.577k',
+    1234, '1.234k', # 3 decimals as rounding gets applied per-class
     748, '748',
     21, '21',
     10, '10',
@@ -19,10 +27,26 @@ test_that('abbreviating formatter works', {
     0, '0'
   )
 
-  x_num <- unlist(x[c(TRUE, FALSE)])
-  x_str <- unlist(x[c(FALSE, TRUE)])
+  run_comparison(x)
+})
 
-  expect_equal(abbrv_large(x_num), x_str)
+test_that('same-scale numbers get truncated to the right decimal place', {
+  x <- c(3.5e9, 3.2e9, 2.97e6, 1.2e3, 1e3)
+  expect_equal(abbrv_large(x), c('3.5B', '3.2B', '2M', '1.2k', '1k'))
+})
+
+test_that('diff-based rounding works', {
+  expect_equal(
+    abbrv_large(c(125234, 126357)), c('125k', '126k')
+  )
+
+  expect_equal(
+    abbrv_large(c(125234, 125357)), c('125.2k', '125.3k')
+  )
+
+  expect_equal(
+    abbrv_large(c(125234, 125257)), c('125.23k', '125.25k')
+  )
 })
 
 test_that('abbreviating formatter accepts NA without barfing', {
