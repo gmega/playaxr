29ac5d57f97b0c117713910a391b11d44b697994
gmega <giuliano.mega@gmail.com>
2020-09-29 13:41:28 -0300

    fix legend key backgrounds, make API more consistent

--- R/theme_playax.R
+++ R/theme_playax.R
@@ -1,3 +1,4 @@
+DEFAULT_FONTSIZE <- 12
 #' Playax theme for ggplot2 charts
 #'
 #' This theme requires Google's Open Sans fonts
@@ -12,20 +13,20 @@
 #'   theme_playax()
 #'
 #' @export
-theme_playax <- function(font_size = 12) {
-  theme_gray() + theme(
-    text = element_text(family = 'Open Sans', size = font_size),
-    axis.text = element_text(size = font_size),
-    axis.title = element_text(size = font_size),
-    legend.text = element_text(size = font_size),
-    panel.background = element_rect(fill = NA, colour = NA),
-    panel.grid.major.y = element_line(size = rel(1), colour = '#e6e6e6'),
-    panel.grid.minor.y = element_blank()
-  )
+theme_playax <- function() {
+  theme_gray() +
+    theme(
+      text = element_text(family = 'Open Sans'),
+      legend.key = element_rect(colour = NA, fill = NA),
+      panel.background = element_rect(fill = NA, colour = NA),
+      panel.grid.major.y = element_line(size = rel(1), colour = '#e6e6e6'),
+      panel.grid.minor.y = element_blank()
+    ) +
+    big_fonts(DEFAULT_FONTSIZE)
 }
 
 #' @export
-big_fonts <- function(n = 12) {
+big_fonts <- function(n = DEFAULT_FONTSIZE) {
   list(
     theme(
       axis.text = element_text(size = n),
