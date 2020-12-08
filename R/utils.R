ba0a7fbe81ec3f9d664b025a8346be0dd7e2327e
gmega <giuliano.mega@gmail.com>
2020-12-08 08:54:22 -0300

    add ggplot2 convenience functions

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,14 @@
+#' @export
+rotate_x_text <- function(angle = 45)
+  theme(axis.text.x = element_text(angle = angle, hjust = 1))
+
+#' @export
+rotate_y_text <- function(angle = 45) theme(
+  axis.text.y = element_text(angle = angle, hjust = 1))
+
+#' @export
+legend_bottom <- function() theme(legend.position="bottom")
+
 if_else <- function(logical, true, false) {
   res <- rep(NA, length(logical))
   lna <- !is.na(logical)
